96b5a99c502d8f14da151ca8d8e3e569d3ad4609
sniperyan <304787171@qq.com>
2017-08-01 17:47:33 +0800

    process

---
+++ process/demo.js
@@ -0,0 +1,25 @@
+/**
+ * process对象是 Node 的一个全局对象，提供当前 Node 进程的信息。
+ * 它可以在脚本的任意位置使用，不必通过require命令加载。该对象部署了EventEmitter接口
+ */
+
+//process.argv：返回一个数组，成员是当前进程的所有命令行参数
+//node xx.js t1 t2
+console.log(process.argv)
+console.log(process.argv.slice(2))  // 取后面参数
+
+//process.env：返回一个对象，成员为当前Shell的环境变量，比如process.env.HOME。
+console.log(process.env)
+
+
+//process.pid：返回一个数字，表示当前进程的进程号。
+console.log(process.pid)
+
+//process.platform：返回一个字符串，表示当前的操作系统，比如Linux。
+console.log(process.platform)
+
+//process.title：返回一个字符串，默认值为node，可以自定义该值。
+console.log(process.title)
+
+//process.version：返回一个字符串，表示当前使用的 Node 版本，比如v7.10.0。
+console.log(process.version)
